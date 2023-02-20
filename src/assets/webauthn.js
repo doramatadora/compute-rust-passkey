fe2c2b57d9f588d654ba3e6d8ab38970c0ba8fa8
Dora Militaru <dora@kalneus.com>
2023-02-20 11:13:01 +0000

    implement start of registration

--- src/assets/webauthn.js
+++ src/assets/webauthn.js
@@ -1,7 +1,16 @@
-const PASSKEY_FORM = document.getElementById("passkeySupported");
+const {
+  browserSupportsWebAuthn,
+  startRegistration,
+  startAuthentication,
+  browserSupportsWebAuthnAutofill,
+} = SimpleWebAuthnBrowser;
+
+const PASSKEY_SUPPORTED = document.getElementById("passkeySupported");
+const PASSKEY_FORM = document.getElementById("passkeyForm");
 const COMPAT_MESSAGE = document.getElementById("passkeyNotSupported");
 const REGISTER_BUTTON = document.getElementById("register");
 const AUTHENTICATE_BUTTON = document.getElementById("authenticate");
+const USER_NAME = document.getElementById("name");
 
 // Availability of `window.PublicKeyCredential` means WebAuthn is usable.
 // `isUserVerifyingPlatformAuthenticatorAvailable` means the feature detection is usable.
@@ -14,7 +23,44 @@ if (
     .then((result) => {
       if (result) {
         // Display form to register or authenticate.
-        PASSKEY_FORM.style.display = "block";
+        PASSKEY_SUPPORTED.style.display = "block";
+        REGISTER_BUTTON.addEventListener("click", async (e) => {
+          e.preventDefault();
+          const apiRegOptsResp = await fetch("/registration/options", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              username: USER_NAME.value,
+            }),
+          });
+          const registrationOptionsJSON = await apiRegOptsResp.json();
+          console.log({registrationOptionsJSON});
+          // Start WebAuthn registration
+          const regResp = await startRegistration(
+            registrationOptionsJSON.publicKey
+          );
+          console.log({regResp});
+          // Submit response
+          const apiRegVerResp = await fetch("/registration/verification", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              username: USER_NAME.value,
+              response: regResp,
+            }),
+          });
+          const verificationJSON = await apiRegVerResp.json();
+
+          // Display outcome
+          if (verificationJSON.verified === true) {
+            alert("Success! Now try to authenticate...");
+          } else {
+            alert(`Registration failed: ${verificationJSON.error}`);
+          }
+        });
+        AUTHENTICATE_BUTTON.addEventListener("click", (e) => {
+          PASSKEY_FORM.action = "/authenticate";
+        });
       } else {
         throw new Error(
           "User verifying platform authenticator is not available."
@@ -27,49 +73,49 @@ if (
     });
 }
 
-const randomStringFromServer = "1234";
-const publicKeyCredentialCreationOptions = {
-  challenge: Uint8Array.from(randomStringFromServer, (c) => c.charCodeAt(0)),
-  rp: {
-    name: "Fastly Compute@Edge",
-    id: "localhost",
-  },
-  user: {
-    id: Uint8Array.from("UZSL85T9AFC", (c) => c.charCodeAt(0)),
-    name: "dora",
-    displayName: "dora",
-  },
-  pubKeyCredParams: [
-    { type: "public-key", alg: -7 },
-    { type: "public-key", alg: -257 },
-  ], // -7 for "ES256" and -257 for "RS256"
-  authenticatorSelection: {
-    authenticatorAttachment: "platform",
-    requireResidentKey: true,
-    // residentKey: "preferred",
-    // requireResidentKey: false,
-    // userVerification: "preferred",
-  },
-  timeout: 60000,
-  attestation: "none",
-  extensions: { credProps: true },
-};
+// const randomStringFromServer = "1234";
+// const publicKeyCredentialCreationOptions = {
+//   challenge: Uint8Array.from(randomStringFromServer, (c) => c.charCodeAt(0)),
+//   rp: {
+//     name: "Fastly Compute@Edge",
+//     id: "localhost",
+//   },
+//   user: {
+//     id: Uint8Array.from("UZSL85T9AFC", (c) => c.charCodeAt(0)),
+//     name: "dora",
+//     displayName: "dora",
+//   },
+//   pubKeyCredParams: [
+//     { type: "public-key", alg: -7 },
+//     { type: "public-key", alg: -257 },
+//   ], // -7 for "ES256" and -257 for "RS256"
+//   authenticatorSelection: {
+//     authenticatorAttachment: "platform",
+//     requireResidentKey: true,
+//     // residentKey: "preferred",
+//     // requireResidentKey: false,
+//     // userVerification: "preferred",
+//   },
+//   timeout: 60000,
+//   attestation: "none",
+//   extensions: { credProps: true },
+// };
 
-navigator.credentials
-  .create({
-    publicKey: publicKeyCredentialCreationOptions,
-  })
-  .then((credential) => {
-    console.log(credential);
-    console.log(credential.response.getPublicKey())
-    // decode the clientDataJSON into a utf-8 string
-const utf8Decoder = new TextDecoder('utf-8');
-const decodedClientData = utf8Decoder.decode(
-    credential.response.clientDataJSON)
+// navigator.credentials
+//   .create({
+//     publicKey: publicKeyCredentialCreationOptions,
+//   })
+//   .then((credential) => {
+//     console.log(credential);
+//     console.log(credential.response.getPublicKey());
+//     // decode the clientDataJSON into a utf-8 string
+//     const utf8Decoder = new TextDecoder("utf-8");
+//     const decodedClientData = utf8Decoder.decode(
+//       credential.response.clientDataJSON
+//     );
 
-// parse the string as an object
-const clientDataObj = JSON.parse(decodedClientData);
+//     // parse the string as an object
+//     const clientDataObj = JSON.parse(decodedClientData);
 
-console.log(clientDataObj)
-
-  });
+//     console.log(clientDataObj);
+//   });
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,56 @@
 use fastly::http::{Method, StatusCode};
 use fastly::{mime, Error, ObjectStore, Request, Response};
+use serde::{Deserialize, Serialize};
 use webauthn_rs::prelude::*;
+// use base64::{
+//     alphabet,
+//     engine::{self, general_purpose},
+//     Engine as _,
+// };
 
 const INDEX_HTML: &str = include_str!("assets/index.html");
 const STYLE_CSS: &str = include_str!("assets/style.css");
 const WEBAUTHN_JS: &[u8] = include_bytes!("assets/webauthn.js");
 
-#[fastly::main]
-fn main(req: Request) -> Result<Response, Error> {
-    let rp_id = "localhost";
-    let rp_origin = Url::parse("http://localhost:7676").expect("Invalid URL");
-    let mut builder = WebauthnBuilder::new(rp_id, &rp_origin).expect("Invalid configuration");
-    let webauthn = builder.build().expect("Invalid configuration");
+// const CUSTOM_ENGINE: engine::GeneralPurpose =
+//     engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 
-    // Initiate a basic registration flow to enroll a cryptographic authenticator
-    let (ccr, skr) = webauthn
-        .start_passkey_registration(Uuid::new_v4(), "claire", "Claire", None)
-        .expect("Failed to start registration.");
+#[derive(serde::Deserialize, serde::Serialize)]
+struct Form {
+    username: String,
+}
+
+// #[derive(serde::Deserialize, serde::Serialize)]
+// struct UserState {
+//     id: Uuid,
+//     reg_state: Option<PasskeyRegistration>,
+// }
+
+#[derive(Deserialize, Serialize)]
+struct UserKeys {
+    keys: Vec<Passkey>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct RegResp {
+    username: String,
+    response: RegisterPublicKeyCredential,
+}
+
+#[fastly::main]
+fn main(mut req: Request) -> Result<Response, Error> {
+    let rp_id = "localhost";
+    let rp_origin = Url::parse("http://localhost:7676").expect("Invalid relying party URL.");
+    let mut builder =
+        WebauthnBuilder::new(rp_id, &rp_origin).expect("Invalid WebAuthn configuration.");
+    let webauthn = builder.build().expect("Invalid WebAuthn configuration.");
+
+    // Username to UUID mapping
+    let mut users = ObjectStore::open("users")?.unwrap();
+    // UUID to registration state mapping
+    let mut state = ObjectStore::open("state")?.unwrap();
+    // UUID to passkeys mapping
+    let mut keys = ObjectStore::open("keys")?.unwrap();
 
     match (req.get_method(), req.get_path()) {
         (&Method::GET, "/robots.txt") => Ok(Response::from_status(StatusCode::OK)
@@ -31,7 +65,75 @@ fn main(req: Request) -> Result<Response, Error> {
         (&Method::GET, "/") => {
             Ok(Response::from_status(StatusCode::OK).with_body_text_html(INDEX_HTML))
         }
+        (&Method::POST, "/registration/options") => {
+            let opts = req.take_body_json::<Form>().unwrap();
+            // 1. Presented credentials may *only* provide the uuid, and not the username!
+            // 2. If the user has any other credentials, we need to exclude these from being re-registered.
+            let (user_id, exclude_credentials) = match users.lookup_str(&opts.username) {
+                Ok(Some(id)) => {
+                    println!("Existing uid for {} is {}", opts.username, id);
+                    match keys.lookup_str(&id) {
+                        Ok(Some(k)) => {
+                            let existing: Vec<Passkey> = serde_json::from_str(&k).unwrap();
+                            (
+                                Uuid::try_parse(&id)?,
+                                Some(
+                                    existing
+                                        .iter()
+                                        .map(|sk| sk.cred_id().clone())
+                                        .collect::<Vec<CredentialID>>(),
+                                ),
+                            )
+                        }
+                        _ => (Uuid::try_parse(&id)?, None),
+                    }
+                }
+                _ => {
+                    let id = Uuid::new_v4();
+                    users
+                        .insert(&opts.username, id.to_string())
+                        .expect("Failed to register new UUID.");
+                    (id, None)
+                }
+            };
 
+            // Initiate a basic registration flow to enroll a cryptographic authenticator
+            let (creation_challenge_response, reg_state) = webauthn
+                .start_passkey_registration(
+                    user_id,
+                    &opts.username,
+                    &opts.username,
+                    exclude_credentials,
+                )
+                .expect("Failed to start registration.");
+
+            // Safe to store the reg_state into the object store since it is not client controlled.
+            // If this was a cookie store, this would be UNSAFE (open to replay attacks).
+            state
+                .insert(&user_id.to_string(), serde_json::to_string(&reg_state)?)
+                .expect("Failed to store registration state.");
+
+            println!(
+                "user_id: {}, name {}, skr {:?}, ccr {:?}",
+                user_id, opts.username, reg_state, creation_challenge_response
+            );
+
+            Ok(Response::from_status(StatusCode::OK)
+                .with_body_json(&creation_challenge_response)?)
+        }
+        (&Method::POST, "/registration/verification") => {
+            let reg = req.take_body_json::<RegResp>().unwrap();
+
+            // Initiate a basic registration flow to enroll a cryptographic authenticator
+            // let (sk) = webauthn
+            //     .finish_passkey_registration(&reg.response, &reg_state)
+            //     .expect("Failed to start registration.");
+
+            println!("registration verification");
+            println!("body {:?}", reg);
+            // webauthn.finish_passkey_registration(reg, state);
+            Ok(Response::from_status(StatusCode::OK))
+        }
         _ => Ok(Response::from_status(StatusCode::NOT_FOUND)),
     }
 }
